4065c48939777adbcc91d446741b29a428d4c838
Kasa28 <agliullinaksenija@gmail.com>
2025-10-10 11:22:24 +0200

    scripts hinzugefügt

---
+++ scripts/login.js
@@ -0,0 +1,10 @@
+ 
+ //prüft ob email und passwort eingegeben wurden
+ 
+ function checkLogin() {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const button = document.getElementById('login_button');
+
+    button.disabled = !(email && password);
+ }
