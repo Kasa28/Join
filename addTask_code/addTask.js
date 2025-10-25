632eb70325b4b4db46cbb1ae8f07fca8b8a8d880
Maya Hamdy <info@ki-konzeptagentur.de>
2025-10-25 09:16:02 +0200

    refactor: add and style dropdwn with choices to category

--- addTask_code/addTask.js
+++ addTask_code/addTask.js
@@ -2,3 +2,6 @@ function toggleAssignDropdown() {
   const dropdown = document.getElementById('assignDropdown');
   dropdown.classList.toggle('active');
 }
+
+const category = document.getElementById('category').value;
+console.log(category); // "technical" oder "user-story"
