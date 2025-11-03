89bc8e33a044beb61bed97cfdeababb49d3ebe90
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2025-11-03 12:36:38 +0100

    include Login
    Include Login

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,74 @@
+const BASE_URL = "https://join-a3ae3-default-rtdb.europe-west1.firebasedatabase.app/";
+users = [];
+
  
+async function getAllUsers(path){
+  let response = await fetch(BASE_URL + path + ".json");
+  return responseToJson = await response.json();
+}
+
+
+async function fillArray(){
+
+   let userResponse = await getAllUsers("users");
+   let UserKeysArray = Object.keys(userResponse);
+
+   for (let index = 0; index < UserKeysArray.length; index++) {
+      users.push(
+         {
+               id : UserKeysArray[index], 
+               user : userResponse[UserKeysArray[index]],
+         }
+
+      )
+      
+   }
+
+   console.log(users);
+   
+
+}
+
+function login(){
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+   checkUsernamePassword(email, password);
+
+
+
+}
+
+
+
+
+function checkUsernamePassword(inputMail, inputPassword){
+   
+   
+   for (let index = 0; index < users.length; index++) {
+
+      console.log(index);
+      
+
+      if(users[index].user.email === inputMail && users[index].user.password === inputPassword){
+            console.log("Login erfolgreich!");
+            return true;
+      }
+   }
+
+   console.log("Login fehlgeschlagen: Benutzer nicht gefunden oder falsches Passwort.");
+   
+   return false;
+}
+
+
+
+
  function checkLogin() {
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
     const button = document.getElementById('login_button');
 
     button.disabled = !(email && password);
+
  }
--- scripts/signUp.js
+++ scripts/signUp.js
@@ -1,17 +1,67 @@
+let users = [];
+let id = 0;
+
 const BASE_URL = "https://join-a3ae3-default-rtdb.europe-west1.firebasedatabase.app/";
 
 
-
-async function postData(path="", data={}) {
-  let response = await fetch(BASE_URL + path + ".json", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
+async function getAllUsers(path){
+  let response = await fetch(BASE_URL + path + ".json");
   return responseToJson = await response.json();
 }
+
+
+async function postDataWithID(path = "", id = "", data = {}){
+  const response = await fetch(`${BASE_URL}${path}/${id}.json`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return await response.json();
+}
+
+
+async function onclickFunction(){
+
+  const name = document.getElementById('name').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  createUser(name , password, email);
+
+  let userResponse = await getAllUsers("users");
+  let UserKeysArray = Object.keys(userResponse);
+
+  for (let index = 0; index < UserKeysArray.length; index++) {
+    users.push(
+          {
+            id : UserKeysArray[index], 
+            user : userResponse[UserKeysArray[index]],
+          }
+      )
+  }
+
+  //console.log(users);
+
+}
+
+async function createUser(inputName, inputPassword, inputMail){
+  
+  let userResponse = await getAllUsers("users");  
+  let UserKeysArray = Object.keys(userResponse);
+
+
+  const user = {name: inputName, password: inputPassword, email: inputMail};
+
+  postDataWithID("users", UserKeysArray.length, user);
+
+
+}
+
+
+
+
 
 
 
@@ -19,22 +69,13 @@ function checkPolicyandAnswers() {
   const name = document.getElementById('name').value.trim();
   const email = document.getElementById('email').value.trim();
   const password = document.getElementById('password').value.trim();
+  const confirmPassword = document.getElementById('confirm_password').value.trim();
   const checkbox = document.getElementById('accept_terms'); 
   const button = document.querySelector('.primary_button'); 
 
   
-  const allFilled = name && email && password &&checkbox.checked;
+  
+  const allFilled = name && email && password && confirmPassword && checkbox.checked;
   button.disabled = !allFilled;
-
-  if(allFilled){
-    postData("users", {username: name, adress: email, key: password});
-  }
-
 }
 
-
-async function loadData(path="", data={}){
-  let response = await fetch(BASE_URL + path + ".json");
-  let responseToJson = await response.json();
-  console.log(responseToJson);
-}
