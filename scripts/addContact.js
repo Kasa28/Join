5bfea8b6a48a36ac0005a26451b8fd682c856691
Ibrahim-Tunca <Ibrahim2234inator@gmail.com>
2025-11-07 15:22:21 +0100

    add
    add some contact stuff

--- scripts/addContact.js
+++ scripts/addContact.js
@@ -21,9 +21,10 @@ function addNewContact(){
     const usermailRef = document.getElementById("add-contact-mailID").value; 
     const phonenumberRef = document.getElementById("add-contact-phone-numberID").value;  
 
-    const contactJson = {"username": usernameRef, "email": usermailRef, "Phone number": phonenumberRef}
+    const contactJson = {"username": usernameRef, "email": usermailRef, "PhoneNumber": phonenumberRef}
 
     contacts.push(contactJson);
+    addContactToLocalStorage(contacts);
 
     console.log(contacts);
 
@@ -33,6 +34,11 @@ function addNewContact(){
 
 
     hideAddContactFormular();
+    renderContactList(contacts);
+}
+
+function addContactToLocalStorage(inputContacts){
+    localStorage.setItem("contacts", JSON.stringify(inputContacts));
 }
 
 function makeFirstLetterBig(inputString){
@@ -41,28 +47,91 @@ function makeFirstLetterBig(inputString){
 
 }
 
+
+
 function renderContactList(){
 
-    const contactContainerRef = document.getElementById("contactContainerID");
+    let contactContainerRef = document.getElementById("contactContainerID");
     contactContainerRef.innerHTML = "";
 
-    contacts.forEach((contact) => {
-    contactContainerRef +=  `
-                    <contact class="single-contact display-flex-center-x padding-medium-up-down-contacts">
+    let getContacts = JSON.parse(localStorage.getItem("contacts"))|| [];
+
+    getContacts.forEach((contact, index) => {
+    contactContainerRef.innerHTML +=  `
+                    <contact onclick=" renderSingleContact(${index})" class="single-contact display-flex-center-x padding-medium-up-down-contacts">
 
                         <div class="contacts-logo">
-                            <a>${contacts.username.charAt(0).toUpperCase()}</a>
+                            <a>${contact.username.charAt(0).toUpperCase()}</a>
                         </div>
                         <div class="padding-left-contacts">
                             <div class="name-property padding-bottom-contacts padding-small-left-right-contacts">
-                                <p>${makeFirstLetterBig(contacts.username)}</p>
+                                <p>${makeFirstLetterBig(contact.username)}</p>
                             </div>
                             <div class="mail-property padding-small-left-right-contacts">
-                                <p>${contacts.usermailRef}</p>
+                                <p>${contact.email}</p>
                             </div>
                         </div>
 
                     </contact>
         `;
     });
+}
+
+
+
+function renderSingleContact(inputIndex){
+    const contacts = JSON.parse(localStorage.getItem("contacts"))|| [];
+    const contact = contacts[inputIndex];
+    const singleContactRef = document.getElementById("singleContactID");
+    singleContactRef.innerHTML = "";
+    singleContactRef.innerHTML = `
+
+    <show-contact>
+
+            <div class="show-contact-container">
+                <contact class="display-flex-center-x">
+                    <div class="show-contact-logo">
+                        <a>${contact.username.charAt(0).toUpperCase()}</a>
+                    </div>
+                    <div>
+                        <div class="name-property">
+                            <h2>${makeFirstLetterBig(contact.username)}</h2>
+                        </div>
+                        <div style="display: flex;">
+                            <div onclick="showEditContactFormular()"  class="edit-delete-container">
+                                <img class="edit-delete-icons" src="./assets/img/edit.svg" alt="edit icon">
+                                Edit
+                            </div>
+                            <div class="edit-delete-container">
+                                <img class="edit-delete-icons" src="./assets/img/delete.svg" alt="delete icon">
+                                Delete
+                            </div>
+                        </div>
+                    </div>
+                </contact>
+                <h2>Contact Information</h2>
+                <br>
+                <h3>E-Mail</h3>
+                <br>
+                <p class="mail-property">${contact.email}</p>
+                <br>
+                <h3>Phone</h3>
+                <br>
+                <p>${contact.PhoneNumber}</p>
+
+            </div>
+
+        </show-contact>
+    
+    `
+}
+
+function editContact(){
+    const contacts = JSON.parse(localStorage.getItem("contacts"))|| [];
+    const contact = contacts[inputIndex];
+
+    const editedUsernameRef = document.getElementById("edit-contact-usernameID").value;
+    const editedUsermailRef = document.getElementById("edit-contact-mailID").value; 
+    const editedPhonenumberRef = document.getElementById("edit-contact-phone-numberID").value;
+
 }
